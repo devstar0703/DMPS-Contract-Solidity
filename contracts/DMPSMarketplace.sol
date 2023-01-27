59143b0d4df2a7e47339b4c1351387494fc8cc13
Milo Thompson <devstar0703@gmail.com>
2023-01-26 21:48:13 -0800

    update feedback

--- contracts/DMPSMarketplace.sol
+++ contracts/DMPSMarketplace.sol
@@ -15,20 +15,13 @@ contract DMPSMarketplace is ERC721URIStorage, Ownable {
 
     address private marketplace_addr ;
 
-    uint256 private royalty_fee = 5 ;
     uint256 private max_supply = 5555 ;
     mapping(uint256 => nft) private nfts ;
 
     struct nft {
         uint256 nft_id ;
-        string nft_name ;
-        string nft_description ;
-        string nft_uri ;
-        uint256 nft_price ;
-        address owner ;
-        address seller ;
-        bool sold ;
-        // uint256 royalty ;
+        uint256 option ;
+        address minter ;
     }
 
     uint256 listing_price = 0.0035 ether ;
@@ -37,44 +30,26 @@ contract DMPSMarketplace is ERC721URIStorage, Ownable {
         marketplace_addr = msg.sender ;
     }
 
-    function mintNft(address minter, string memory name, string memory description, string memory uri, uint256 price) public returns(uint256) {
+    function mintNFT(address minter, uint256 option) public payable returns(uint256) {
+        require(msg.value == listing_price * option, "mintNFT: insufficient value");
+        require(nft_counter.current() < max_supply, "mintNFT: overflow supply") ;
+
         nft_counter.increment() ;
 
         uint256 new_nft_id = nft_counter.current() ;
 
         _safeMint(minter, new_nft_id) ;
-        _setTokenURI(new_nft_id, uri);
+        // _setTokenURI(new_nft_id, uri);
 
         nfts[new_nft_id] = nft(
             new_nft_id,
-            name,
-            description,
-            uri,
-            price,
-            marketplace_addr,
-            minter,
-            false
+            option,
+            msg.sender
         );
 
         _transfer(minter, marketplace_addr, new_nft_id);
 
         return new_nft_id ;
-    }
-
-    function mintNftPayable(string memory name, string memory description, string memory uri, uint256 price) public payable {
-        require(price > 0, "mintNftPayable: price is too low");
-        require(msg.value == listing_price, "mintNftPayable: msg.value != listing_price") ;
-        require(nft_counter.current() < max_supply, "mintNftPayable: overflow supply");
-
-        mintNft(msg.sender, name, description, uri, price);
-        
-    }
-
-    function mintNftFree(string memory name, string memory description, string memory uri, uint256 price) public {
-        require(price > 0, "mintNftFree: price is too low");
-        require(nft_counter.current() < max_supply, "mintNftFree: overflow supply");
-
-        mintNft(msg.sender, name, description, uri, price);
     }
 
     function fetchNFTs() public view returns(nft[] memory) {
